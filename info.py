ed90b8246097f50a8a891c129de30d630727a4e3
Gavesh Bimsath <91552385+gbimsath@users.noreply.github.com>
2022-12-11 21:02:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,9 +32,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = mongodb+srv://imdb:imdb321@imdbbot.ghhs6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+DATABASE_NAME = imdb
+COLLECTION_NAME = MoveHubLK_Files
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
